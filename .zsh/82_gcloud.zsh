122452ab752f4cf33a9f2b75ea79be6bf85c3ccc
gustin prudner <gustin@users.noreply.github.com>
2017-02-12 14:16:25 -0500

    Add google cloud setup

---
+++ .zsh/82_gcloud.zsh
@@ -0,0 +1,9 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f /Users/gustin/Dev/google-cloud-sdk/path.zsh.inc ]; then
+  source '/Users/gustin/Dev/google-cloud-sdk/path.zsh.inc'
+fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f /Users/gustin/Dev/google-cloud-sdk/completion.zsh.inc ]; then
+  source '/Users/gustin/Dev/google-cloud-sdk/completion.zsh.inc'
+fi
