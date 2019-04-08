d138f761efdfe2de28a7b9f566c211f69e8a8177
gustin <gustin@users.noreply.github.com>
2019-04-08 13:37:22 -0400

    Set GPG terminal.

--- .zsh/20_environment.zsh
+++ .zsh/20_environment.zsh
@@ -3,15 +3,13 @@ export GREP_COLOR='1;32'
 export EDITOR='vi'
 export CFLAGS="-I$(brew --prefix openssl)/include"
 export LDFLAGS="-L$(brew --prefix openssl)/lib"
+export GPG_TTY=$(tty)
 
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/Users/gustin/.local/bin:$PATH"
 
-export NVM_DIR="$HOME/.nvm"
-. "/usr/local/opt/nvm/nvm.sh"
+#export NVM_DIR="$HOME/.nvm"
+#. "/usr/local/opt/nvm/nvm.sh"
 
-. $HOME/.asdf/asdf.sh
-
-. $HOME/.asdf/completions/asdf.bash
