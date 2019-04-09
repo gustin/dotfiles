d8d9f962d663b7817c5d9564f3bc590548785ec3
gustin <gustin@users.noreply.github.com>
2019-04-09 11:06:45 -0400

    Remove unneeded tools, add asdf env.

--- .zsh/90_custom_environment.zsh
+++ .zsh/90_custom_environment.zsh
@@ -1,16 +1,9 @@
 export SSL_CERT_FILE='/usr/local/etc/openssl/cert.pem'
-export HBASE_HOME=/usr/local/Cellar/hbase/1.2.6/libexec
-export HADOOP_HOME=/usr/local/Cellar/hadoop/2.8.0
 
 export PATH="/usr/local/opt/curl/bin:$PATH"
 export PATH="/usr/local/opt/openssl/bin:$PATH"
-export PATH="/usr/local/heroku/bin:$PATH"
-export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH="/usr/local/share/npm/bin:$PATH"
-export PATH="$HOME/.jenv/bin:$PATH"
-export PATH="$HOME/usr/local/Cellar/apache-spark/1.4.0/bin:$PATH"
-export PATH="$HOME/anaconda2/bin:$PATH"
-export PATH="$HOME/Library/Python/2.7/bin:$PATH"
-export PATH="$HOME/Library/Python/3.6/bin:$PATH"
+export PATH="$PATH:$HOME/.bin/"
 export PATH="$PATH:/usr/local/opt/go/libexec/bin"
-export NVM_DIR="$HOME/.nvm"
+
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
