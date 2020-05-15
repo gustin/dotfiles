7e180012a3caa243c739b20fef0f177aa54742e0
gustin <7021+gustin@users.noreply.github.com>
2020-05-14 21:43:42 -0400

    Fix gpg-agent check to only run once.

--- .zsh/21_gpg.zsh
+++ .zsh/21_gpg.zsh
@@ -1,7 +1,16 @@
-export GPG_TTY=$(tty)
-[ -f ~/.gnupg/gpg-agent-info ] && source ~/.gnupg/gpg-agent-info
-if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
-      export GPG_AGENT_INFO
-    else
-      eval $( gpg-agent --daemon --options ~/.gnupg/gpg-agent.conf )
+# Enable gpg-agent if it is not running-
+# # --use-standard-socket will work from version 2 upwards
+#
+AGENT_SOCK=`gpgconf --list-dirs | grep agent-socket | cut -d : -f 2`
+
+if [ ! -S ${AGENT_SOCK} ]; then
+  gpg-agent --daemon --use-standard-socket >/dev/null 2>&1
 fi
+export GPG_TTY=$(tty)
+
+# Set SSH to use gpg-agent if it's enabled
+if [ -S "${AGENT_SOCK}.ssh" ]; then
+  export SSH_AUTH_SOCK="${AGENT_SOCK}.ssh"
+  unset SSH_AGENT_PID
+fi
+
