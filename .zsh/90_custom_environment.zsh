454afd977c6bd59d42efc32bdece616e70879606
gustin <7021+gustin@users.noreply.github.com>
2020-05-14 21:45:30 -0400

    Fix SSL pathing.

--- .zsh/90_custom_environment.zsh
+++ .zsh/90_custom_environment.zsh
@@ -1,9 +1,13 @@
 export SSL_CERT_FILE='/usr/local/etc/openssl/cert.pem'
 
 export PATH="/usr/local/opt/curl/bin:$PATH"
+export PATH="$HOME/.volta/bin/:$PATH"
 export PATH="/usr/local/opt/openssl/bin:$PATH"
 export PATH="$PATH:$HOME/.bin/"
 export PATH="$PATH:/usr/local/opt/go/libexec/bin"
+
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
 
 . $HOME/.asdf/asdf.sh
 . $HOME/.asdf/completions/asdf.bash
