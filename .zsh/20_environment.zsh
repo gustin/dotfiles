62f4b2f9e69b11ab24b51eca6c57026c0297f5d7
gustin <7021+gustin@users.noreply.github.com>
2025-07-16 18:56:37 -0400

    Update 20_environment.zsh

--- .zsh/20_environment.zsh
+++ .zsh/20_environment.zsh
@@ -3,7 +3,7 @@ export GREP_COLOR='1;32'
 export EDITOR='vi'
 
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-MANPATH=""/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/Users/gustin/.local/bin:$PATH"
